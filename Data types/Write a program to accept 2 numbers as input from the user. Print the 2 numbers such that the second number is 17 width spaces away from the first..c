954f5f770a54e7e913e12948c4915763e7b181de
HayanthikaK <121630073+HayanthikaK@users.noreply.github.com>
2025-07-03 14:39:17 +0530

    Add files via upload

---
+++ Data types/Write a program to accept 2 numbers as input from the user. Print the 2 numbers such that the second number is 17 width spaces away from the first..c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int x, y;
+    scanf("%d %d",&x,&y);
+    printf("%d%017d\n",x,y);
+    return 0;
+}
