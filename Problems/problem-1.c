248c79ede15134b361a92f2fbe09d08faca6b24c
HayanthikaK <121630073+HayanthikaK@users.noreply.github.com>
2026-02-20 09:52:10 +0530

    Created Partial_destruct.rs

---
+++ Problems/problem-1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+    int start, end, divisor, i;
+
+    printf("Enter start number: ");
+    scanf("%d", &start);
+
+    printf("Enter end number: ");
+    scanf("%d", &end);
+
+    printf("Enter divisor: ");
+    scanf("%d", &divisor);
+
+    printf("Numbers divisible by %d between %d and %d are:\n", divisor, start, end);
+
+    for (i = start; i <= end; i++) {
+        if (i % divisor == 0) {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
